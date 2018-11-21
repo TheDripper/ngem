bad8c15e4bff5bbd6d5af64314d95e0df58db715
TheDripper <tylerehill@gmail.com>
2018-11-21 14:47:15 -0800

    nav

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+<nav class="navbar cont" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="/logo.png" width="112" height="28">
+    </a>
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+    </div>
+    <div class="navbar-end">
+    <a class="navbar-item">Home</a>
+    <a class="navbar-item">Contact</a>
+    <a class="navbar-item">Client Login</a>
+    <div class="navbar-item">
+    	<form id=search action="/search">
+		<div class="field">
+		  <div class="control">
+		    <input class="input" type="text" placeholder="search...">
+		  </div>
+		</div>
+	</form>
+    </div>
+    </div>
+  </div>
+</nav>
+    
+
+    <nuxt/>
+  </div>
+</template>
+<style>
+.cont {
+	width: 100%;
+	max-width: 1140px;
+	margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="columns cont">
+	<div class="column is-three-quarters">
+	<h1>three</h1>
+	</div>
+	<div class="column is-one-quarter">
+	<h1>one</h1>
+	</div>
+</div>
+  
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
